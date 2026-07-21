068bb4dd99ce9e1c0a050ce6dd3589b36689ff1f
nsmangat <nsmangat990@gmail.com>
2026-07-20 21:07:33 -0400

    Setup frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-950">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-white">Tailwind test</h1>
+        <button className="mt-4 rounded-lg bg-blue-700 px-4 py-2 text-white hover:bg-blue-500">
+          Test
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
